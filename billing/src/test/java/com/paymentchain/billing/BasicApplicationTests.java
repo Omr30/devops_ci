2de469255368f83df5e5b1d2e9614c184ab5a0f3
mvrk <oscarmenacho23@gmail.com>
2023-03-17 11:26:37 -0500

    add integrating test

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
  
  @Test
  public void contextLoads() {
- 	    String meessage= "probando el pipeline con jenkins";
+ 	    String meessage= "probando el pipeline con jenkins y haciendo merge";
  	    Assert.assertNotNull(meessage);
  }
  
