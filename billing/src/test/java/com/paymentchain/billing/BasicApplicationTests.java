236de366d1988c13b987769c61e2a32403422b2f
mvrk <oscarmenacho23@gmail.com>
2023-03-17 19:27:57 -0500

    add change using slack notification

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -12,7 +12,7 @@ public class BasicApplicationTests {
  
  @Test
  public void contextLoads() {
- 	    String meessage= "probando el pipeline con jenkins y haciendo merge";
+ 	    String meessage= "probando el pipeline con jenkins y haciendo merge probando slack";
  	    Assert.assertNotNull(meessage);
  }
  
